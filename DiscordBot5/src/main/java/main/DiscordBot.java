f0c86497f076d675810aa690a517237801b94c33
Rian30 <55314284+Rian30@users.noreply.github.com>
2020-12-13 17:32:03 +0100

    :new: Added DiscordBot Class
    Central Discord Bot Class which logs-in and starts Listeners

---
+++ DiscordBot5/src/main/java/main/DiscordBot.java
@@ -0,0 +1,34 @@
+package main;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class DiscordBot {
+
+    private final String propFileName = "./src/data/config.properties";
+
+    private JDA jda;
+
+    public DiscordBot() throws LoginException, IOException {
+        InputStream inputStream = new FileInputStream(propFileName);
+        System.out.println(inputStream);
+        Properties prop = new Properties();
+        prop.load(inputStream);
+
+
+
+        jda = JDABuilder
+                .createDefault((String)prop.get("DISCORD_API_KEY"))
+                .build();
+
+        jda.addEventListener(new PingCmd());
+        jda.addEventListener(new TheoremDesTages(jda));
+    }
+
+}
