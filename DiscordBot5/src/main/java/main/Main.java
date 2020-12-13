42cbe40a4e99ab777d26d3ae92cf73dff614e465
Rian30 <55314284+Rian30@users.noreply.github.com>
2020-12-13 17:08:24 +0100

    :new: Added Main class
    Starting Discord Bot

---
+++ DiscordBot5/src/main/java/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    public static void main(String[] args) throws LoginException,Exception {
+        new DiscordBot();
+    }
+
+}
