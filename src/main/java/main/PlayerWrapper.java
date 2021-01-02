646ca780275ab1909cfd65740d55fd29e38f1346
nonchris <git@chris-ge.de>
2021-01-02 18:17:46 +0100

    :new: Added functionality for playback
    Added class AudioPlayerSendHandler to link AudioPlayer and AudioManger.
    Also added function `play()` for loading file and starting playback
    
    Co-Authored-By: Rian30 <55314284+Rian30@users.noreply.github.com>

--- src/main/java/main/PlayerWrapper.java
+++ src/main/java/main/PlayerWrapper.java
@@ -15,6 +15,34 @@ import java.nio.ByteBuffer;
 
 
 public class PlayerWrapper {
+
+    //handles connection between audio player and voice channel
+    //(der Rest is trivial können sie als Übung ja selbst einmal machen)
+    private class AudioPlayerSendHandler implements AudioSendHandler {
+        private final AudioPlayer audioPlayer;
+        private AudioFrame lastFrame;
+
+        public AudioPlayerSendHandler(AudioPlayer audioPlayer) {
+            this.audioPlayer = audioPlayer;
+        }
+
+        @Override
+        public boolean canProvide() {
+            lastFrame = audioPlayer.provide();
+            return lastFrame != null;
+        }
+
+        @Override
+        public ByteBuffer provide20MsAudio() {
+            return ByteBuffer.wrap(lastFrame.getData());
+        }
+
+        @Override
+        public boolean isOpus() {
+            return true;
+        }
+    }
+
     AudioPlayerManager playerManager;
     AudioPlayer player;
 
@@ -32,4 +60,34 @@ public class PlayerWrapper {
         player.addListener(eventAdapter);
     }
 
+    public void play(AudioManager audioManager) {
+
+        audioManager.setSendingHandler(new AudioPlayerSendHandler(player));
+
+        eventAdapter.setAudioManager(audioManager); //for closing the connection
+
+        //loading track and passing it to audio player
+        playerManager.loadItem("./src/data/greeting.mp3", new AudioLoadResultHandler() {
+            @Override
+            public void trackLoaded(AudioTrack audioTrack) {
+                player.playTrack(audioTrack);
+            }
+
+            @Override
+            public void playlistLoaded(AudioPlaylist audioPlaylist) {
+                System.out.println("list loaded");
+            }
+
+            @Override
+            public void noMatches() {
+                System.out.println("no matches");
+            }
+
+            @Override
+            public void loadFailed(FriendlyException e) {
+                System.out.println("Load Failed");
+            }
+        });
+    }
 }
+
