b66749f5fc65e9be4c1a84295823777a3d2cb6ab
nonchris <git@chris-ge.de>
2021-01-03 18:45:00 +0100

    :arrow_upper_right: Removed command handling and refactored joinChannel
    Commands are now handled in CommandHandler.
    -> Removed handling from this class
    
    Co-Authored-By: Rian30 <55314284+Rian30@users.noreply.github.com>

--- src/main/java/main/VoiceChannelHandler.java
+++ src/main/java/main/VoiceChannelHandler.java
@@ -1,51 +1,29 @@
 package main;
 
 import net.dv8tion.jda.api.entities.*;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.managers.AudioManager;
 
-public class VoiceChannelHandler extends ListenerAdapter {
+public class VoiceChannelHandler {
 
-    PlayerWrapper playerWrapper = new PlayerWrapper();
+    private static PlayerWrapper playerWrapper = new PlayerWrapper();
 
-    @Override
-    public void onMessageReceived(MessageReceivedEvent event){
-        //ignores other bots and self
-        if(event.getAuthor().isBot()) return;
-
-        //gets message content and checks for command prefix
-        Message message = event.getMessage();
-        String content = message.getContentRaw();
-
-        if(!content.startsWith(DiscordBot.prefix)) return;
-
-        //checking for valid command
-        if(content.contains("join")) {
-            joinChannel(event.getGuild(), event.getChannel(), event.getMember().getVoiceState().getChannel());
+    public static void joinChannel(VoiceChannel voiceChannel) throws NoVoiceChannelError {
+        //returns if user is not in voice channel
+        if(voiceChannel == null) {
+            throw new NoVoiceChannelError();
         }
-        else if(content.contains("leave")) {
-            event.getGuild().getAudioManager().closeAudioConnection();
-        }
-    }
 
-    private void joinChannel(Guild guild, MessageChannel channel, VoiceChannel connectedChannel) {
+        Guild guild = voiceChannel.getGuild();
+
         //returns if bot is already in voice channel
         if(guild.getSelfMember().getVoiceState().inVoiceChannel()) return;
-        System.out.println("Connected");
-
-        //returns if user is not in voice channel
-        if(connectedChannel == null) {
-            channel.sendMessage("You are not connected to a voice channel!").queue();
-            return;
-        }
 
         //getting audio manager
         AudioManager audioManager = guild.getAudioManager();
 
         //connecting to channel
-        audioManager.openAudioConnection(connectedChannel);
-        System.out.println("Joined Channel: " + connectedChannel.getName());
+        audioManager.openAudioConnection(voiceChannel);
+        System.out.println("Joined Channel: " + voiceChannel.getName());
 
         //starts audio
         playerWrapper.play(audioManager);
