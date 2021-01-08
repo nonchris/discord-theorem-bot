a6d13f618b7bc3cb5fae06d637bfe774d5daa642
Rian30 <55314284+Rian30@users.noreply.github.com>
2021-01-08 22:15:07 +0100

    :arrow_upper_right: Used TheoremHandler to pick theorems and added play comand
    Picks theorem in join that should be played and starts them.
    If play Command is called and bot is in voice channel the bot adds the given amount of theorems to the playlist

--- src/main/java/main/Commands.java
+++ src/main/java/main/Commands.java
@@ -2,7 +2,12 @@ package main;
 
 import main.exceptions.NoVoiceChannelError;
 import main.exceptions.NotInThisVoiceChannelException;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.VoiceChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.io.IOException;
+import java.util.List;
 
 
 public class Commands {
@@ -15,13 +20,56 @@ public class Commands {
         ch.addCommand("join", event -> {
             //handles exception if user isn't in a voice channel
             try  {
+                int theoremAmount = TheoremHandler.getTheoremAmount(event.getMessage().getContentRaw());
+                List<String> theoremList = TheoremHandler.generateTheoreme(theoremAmount);
+                theoremList.add(0,"./src/data/greeting.mp3");
+                String[] theoremArr = theoremList.stream().toArray(String[]::new);
+
                 VoiceChannel channel = event.getMember().getVoiceState().getChannel();
                 VoiceChannelHandler.joinChannel(channel);
+
                 AudioHandlerWrapper.addVoiceChannel(channel);
-                AudioHandlerWrapper.playTrack(channel,"./src/data/greeting.mp3","./src/data/ending.mp3");
+                AudioHandlerWrapper.playTrack(channel,theoremArr);
+
             }catch(NoVoiceChannelError e){
                 event.getChannel().sendMessage("You are not in a voice channel").queue();
+            }catch(IOException e){
+                System.out.println("could not load file");
+            }catch(IllegalArgumentException e){
+                event.getChannel().sendMessage("The was something wrong with you arguments").queue();
             }
+        });
+
+        //command to add specified amount of theorems to playlist
+        ch.addCommand("play",event -> {
+            int theoremAmount;
+            try {
+                //gets amount of theorems and checks if command attributes are valid
+                theoremAmount = TheoremHandler.getTheoremAmount(event.getMessage().getContentRaw());
+
+                //Checks if bot is in same voiceChannel as calling Member
+                VoiceChannel voiceChannel = event.getMember().getVoiceState().getChannel();
+                VoiceChannelHandler.checkVoiceChannel(voiceChannel);
+
+                //Gets Paths to theorems and plays them
+                List<String> theoremList = TheoremHandler.generateTheoreme(theoremAmount);
+                String[] theoremArr = theoremList.stream().toArray(String[]::new);
+                AudioHandlerWrapper.playTrack(voiceChannel,theoremArr);
+
+            } catch (IOException e) {
+                return;
+            }catch(NoVoiceChannelError e) {
+                event.getChannel().sendMessage("You are not in a voice channel").queue();
+                return;
+            }catch(NotInThisVoiceChannelException e){
+                event.getChannel().sendMessage("Im not in your voice channel").queue();
+                return;
+            }catch(IllegalArgumentException e){
+                event.getChannel().sendMessage("The was something wrong with you arguments").queue();
+                return;
+            }
+
+            event.getChannel().sendMessage("Added " + theoremAmount + " theoreme to the playlist").queue();
         });
 
         //command for leaving a voice channel
