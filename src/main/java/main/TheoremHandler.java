80df3ae882ec20b4bb6a524211739bec80597af5
nonchris <git@chris-ge.de>
2021-01-10 23:29:00 +0100

    :arrow_upper_right: Added check to generate new mp3 files
    This function checks wether a mp3 file does already exist, if that's not the case we'll generate it calling text to speech.
    
    Co-Authored-By: thisryan <55314284+thisryan@users.noreply.github.com>

--- src/main/java/main/TheoremHandler.java
+++ src/main/java/main/TheoremHandler.java
@@ -1,5 +1,7 @@
 package main;
 
+import com.google.cloud.texttospeech.v1.SsmlVoiceGender;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -21,6 +23,9 @@ public class TheoremHandler {
     public static List<String> generateTheoreme(int amount) throws IOException {
         //Gets a List of all available Theorems
         List<Path> paths = Files.list(Paths.get(theoremPath)).collect(Collectors.toList());
+        //Filter all mp3 files
+        paths = paths.stream().filter(path -> path.toString().contains(".txt")).collect(Collectors.toList());
+
         List<Path> takenPaths = new ArrayList<>();
         List<String> theoreme = new ArrayList<>();
 
@@ -48,7 +53,53 @@ public class TheoremHandler {
             }
         }
 
+        theoreme = theoreme.stream().map(path -> path.replace("txt","mp3")).collect(Collectors.toList());
+
+        checkFiles(theoreme);
+
         return theoreme;
+    }
+
+    //function that checks if all chosen theorems have associated audio files
+    private static void checkFiles(List<String> theorems) {
+        //loops through all given theorems
+        for(int i = 0;i < theorems.size();i++){
+            String path = theorems.get(i);
+            //if file does not exists create it
+            if(!Files.exists(Paths.get(path))){
+                //gets theorem from text file
+                String content = "";
+                try {
+                    content = Files.readAllLines(Paths.get(path.replace("mp3", "txt"))).stream().collect(Collectors.joining());
+                } catch(IOException e){
+                    //if we can't find the text file just remove the theorem
+                    theorems.remove(i);
+                    continue;
+                }
+
+                //Creates new Text to speech handler
+                TextToSpeechHandler textToSpeech = new TextToSpeechHandler();
+
+                //set the text that should be converted to TTS
+                textToSpeech.setMessage(content);
+
+                //sets the spoken language to german
+                textToSpeech.setLanguageCode("de-DE");
+
+                //sets the voice gender to male
+                textToSpeech.setSsmlGender(SsmlVoiceGender.MALE);
+
+                //trying to generate audio with google TTS
+                //removing entry if TTS fails
+                try{
+                    textToSpeech.generateMP3(path);
+                }catch(IOException e){
+                    theorems.remove(i);
+                }
+
+
+            }
+        }
     }
 
     //Function for getting the amount of theorems that should be played
@@ -66,6 +117,4 @@ public class TheoremHandler {
 
         return number;
     }
-
-
 }
